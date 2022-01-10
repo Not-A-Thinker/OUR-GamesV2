cfa5d567a58bb36b7d7d21be98b8eb2d57565b3e
Yung61 <j6mp6g@gmail.com>
2022-01-10 16:10:56 +0800

    Feather Prefab update
    Feather Prefab update

--- S4Unit3/Assets/Scripts/Bullet.cs
+++ S4Unit3/Assets/Scripts/Bullet.cs
@@ -4,33 +4,57 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    [SerializeField][Header("幫我在攻擊的時候打開")]
+    public bool isAttacking = false;
     public GameObject tailParticle;
     public Quaternion dir;
-    // Start is called before the first frame update
     void Start()
     {
         dir = dir == null ? Quaternion.identity : dir;
         tailParticle.transform.rotation = dir;
+        tailParticle.SetActive(false);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(isAttacking)
+        {
+            tailParticle.SetActive(true);
+        }
     }
 
-    public Transform explosionPrefab;
+   // public Transform explosionPrefab;
     public GameObject hitParticle;
-    private void OnCollisionEnter(Collision collision)
-    {
-        ContactPoint contact = collision.contacts[0]; //get hit point 
-        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal); // rotation
-        Vector3 pos = contact.point;//position
 
-      //Instantiate(explosionPrefab, pos, rot);
-        GameObject obj = Instantiate(hitParticle, pos, Quaternion.identity);
-        Destroy(obj, 5);
-        Destroy(gameObject);
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    print("collision with " + collision.gameObject.name);
+    //    if(collision.transform.gameObject.tag=="Boss")
+    //    {
+    //        ContactPoint contact = collision.contacts[0]; //get hit point 
+    //        Quaternion rot = Quaternion.FromToRotation(Vector3.up, contact.normal); // rotation
+    //        Vector3 pos = contact.point;//position
+
+    //        //Instantiate(explosionPrefab, pos, rot);
+    //        GameObject particleobj = Instantiate(hitParticle, pos, Quaternion.identity);
+    //        Destroy(particleobj, 5);
+    //    }
+    //  //  Destroy(gameObject);
+    //}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Boss")
+        {
+            if(isAttacking)
+            {
+                GameObject particleobj = Instantiate(hitParticle, transform.position, Quaternion.identity);
+
+                Destroy(particleobj, 5);
+            }
+        }
     }
 
 
