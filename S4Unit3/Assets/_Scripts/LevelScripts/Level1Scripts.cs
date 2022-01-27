67d86a3936c12d9f7d6f384a79968a5de68c64e6
Kleon <kleon2070310@gmail.com>
2022-01-27 16:50:03 +0800

    Crystal cubic changed

--- S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
+++ S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
@@ -17,7 +17,8 @@ public class Level1Scripts : MonoBehaviour
 
         foreach (var crystal in _crystalAlter)
         {
-            crystal.DOMoveY(_crystalHeight, Random.Range(1.8f, 2f)).SetLoops(-1, LoopType.Yoyo);
+            crystal.DOMoveY(_crystalHeight, Random.Range(1.8f, 2f)).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutCubic);
+            crystal.DORotate(new Vector3(360, 360, 360), 2, RotateMode.FastBeyond360).SetLoops(-1, LoopType.Restart).SetEase(Ease.Linear);
         }
     }
 
