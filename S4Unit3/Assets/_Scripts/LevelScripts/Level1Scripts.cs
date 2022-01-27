884ca630caa4e503bf636cb8685880497cd4c36b
Kleon <kleon2070310@gmail.com>
2022-01-27 16:02:00 +0800

    Crystal Flow Testing

--- S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
+++ S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 
 //This Script should be use as the whole level control.
 public class Level1Scripts : MonoBehaviour
 {
-    
-
+    [SerializeField] Transform[] _crystalAlter;
+    [Tooltip("This is use for controlling the height of crystals can flow based on LevelHandler.")]
+    [SerializeField] float _crystalHeight = 6f;
+        
 
     void Start()
     {
-        
+        var sequence = DOTween.Sequence();
+
+        foreach (var crystal in _crystalAlter)
+        {
+            crystal.DOMoveY(_crystalHeight, Random.Range(1.8f, 2f)).SetLoops(-1, LoopType.Yoyo);
+        }
     }
 
     
