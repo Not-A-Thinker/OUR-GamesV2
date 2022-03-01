1a9c23dc90f868532bf77a5e9388fac85725c364
Yung61 <j6mp6g@gmail.com>
2022-03-01 14:41:31 +0800

    改了好多咚咚....

---
+++ S4Unit3/Assets/_Scripts/ParticleScripts/Particle_Hit_WithPlayer.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle_Hit_WithPlayer : MonoBehaviour
+{
+    public GameObject hitParticle;
+    [Header("§ðÀ»¥Ø¼Ð(player)")]
+    public GameObject Traget;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag=="Player" || collision.gameObject== Traget)
+        {
+            GameObject particleobj = Instantiate(hitParticle, collision.gameObject.transform.position, Quaternion.identity);
+            Destroy(particleobj, 5);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" || other.gameObject == Traget)
+        {
+            print("ddddd");
+            GameObject particleobj = Instantiate(hitParticle, other.gameObject.transform.position, Quaternion.identity);
+            Destroy(particleobj, 5);
+        }
+    }
+}
