54bbdf7b32be98a3e16c5ee7774c8365711db771
Yung61 <j6mp6g@gmail.com>
2022-03-11 14:30:50 +0800

    先推一個版本
    03/10   Volume&WindHole&Tornado Special Attack

--- S4Unit3/Assets/_Scripts/ParticleScripts/Particle_Hit_WithPlayer.cs
+++ S4Unit3/Assets/_Scripts/ParticleScripts/Particle_Hit_WithPlayer.cs
@@ -6,7 +6,7 @@ public class Particle_Hit_WithPlayer : MonoBehaviour
 {
     public GameObject hitParticle;
 
-    public bool Staying = false;
+    bool Staying = false;
     // Start is called before the first frame update
     void Start()
     {
