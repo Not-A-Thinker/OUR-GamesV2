c6c96bddfd5ce585e8878a47cda29e3104e1ef63
Kleon <kleon2070310@gmail.com>
2022-03-19 21:11:03 +0800

    backup sum din
    fuck i cant get event work

--- S4Unit3/Assets/_System/Boss/Scripts/BossDamageIndicator.cs
+++ S4Unit3/Assets/_System/Boss/Scripts/BossDamageIndicator.cs
@@ -1,19 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class BossDamageIndicator : MonoBehaviour
 {
+    
     public Material material;
+
+    public UnityEvent OnDamageEvent;
+
+    void Awake()
+    {
+        OnDamageEvent.AddListener(ColorChangeTrigger);
+    }
 
     void Start()
     {
-        material.color = Color.red;
+        material.SetColor("MainColor", new Color(1,1,1));
     }
 
 
     void Update()
     {
         
+    }
+
+    public void ColorChangeTrigger()
+    {
+        StartCoroutine(ColorChange());
+    }
+
+    public IEnumerator ColorChange()
+    {
+        material.SetColor("MainColor", Color.red);
+        yield return new WaitForSeconds(0.5f);
+        material.SetColor("MainColor", new Color(1, 1, 1));
     }
 }
