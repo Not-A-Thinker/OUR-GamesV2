d0ffdadea5e8a668184f1b07657216db5b585e93
Kleon <kleon2070310@gmail.com>
2022-03-20 16:09:12 +0800

    Update BossDamageIndicator.cs
    Now only left the event step to make indicator works...
    or maybe i can use a stupid method first

--- S4Unit3/Assets/_System/Boss/Scripts/BossDamageIndicator.cs
+++ S4Unit3/Assets/_System/Boss/Scripts/BossDamageIndicator.cs
@@ -5,8 +5,14 @@ using UnityEngine.Events;
 
 public class BossDamageIndicator : MonoBehaviour
 {
-    
     public Material material;
+    [SerializeField] Color colorTemp;
+    [SerializeField] Color colorNormal;
+    [SerializeField] Color colorDamage;
+
+    [Tooltip("Value smaller mean faster.")]
+    [SerializeField] float smoothing = .5f;
+    float elapsedTime;
 
     public UnityEvent OnDamageEvent;
 
@@ -18,12 +24,39 @@ public class BossDamageIndicator : MonoBehaviour
     void Start()
     {
         material.SetColor("MainColor", new Color(1,1,1));
+        Debug.Log(material.GetColor("MainColor"));
+        colorTemp = material.GetColor("MainColor");
     }
 
 
     void Update()
     {
-        
+        //if (Input.GetKeyDown(KeyCode.K))
+        //{
+        //    ColorValueChange();
+        //}
+
+        if (colorTemp != colorNormal)
+        {
+            Debug.Log("Is changing!");
+            elapsedTime += Time.deltaTime;
+            float percentageComplete = elapsedTime / smoothing;
+
+            float r = Mathf.Lerp(colorDamage.r, colorNormal.r, percentageComplete);
+            float g = Mathf.Lerp(colorDamage.g, colorNormal.g, percentageComplete);
+            float b = Mathf.Lerp(colorDamage.b, colorNormal.b, percentageComplete);
+            colorTemp = new Color(r, g, b);
+
+            material.SetColor("MainColor", colorTemp);
+        }
+    }
+
+    public void ColorValueChange()
+    {
+        material.SetColor("MainColor", colorDamage);
+        colorTemp = material.GetColor("MainColor");
+
+        elapsedTime = 0;
     }
 
     public void ColorChangeTrigger()
@@ -34,7 +67,10 @@ public class BossDamageIndicator : MonoBehaviour
     public IEnumerator ColorChange()
     {
         material.SetColor("MainColor", Color.red);
+        colorTemp = material.GetColor("MainColor");
         yield return new WaitForSeconds(0.5f);
         material.SetColor("MainColor", new Color(1, 1, 1));
     }
+
+
 }
