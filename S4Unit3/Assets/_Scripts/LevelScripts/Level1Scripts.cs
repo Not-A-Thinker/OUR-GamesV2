955a0cd8aa80291f6a5ed34d9fe29748d708a00d
Kleon <kleon2070310@gmail.com>
2022-04-09 13:13:03 +0800

    I make the crys move
    move bitch

--- S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
+++ S4Unit3/Assets/_Scripts/LevelScripts/Level1Scripts.cs
@@ -9,6 +9,22 @@ public class Level1Scripts : MonoBehaviour
     [SerializeField] Transform[] _crystalAlter;
     [Tooltip("This is use for controlling the height of crystals can flow based on LevelHandler.")]
     [SerializeField] float _crystalHeight = 6f;
+    [Space]
+    [SerializeField] Transform[] _crystalOuterAlter;
+    [Tooltip("This is use for controlling the height of the outer crystals can flow based on LevelHandler.")]
+    [SerializeField] float _crystalOuterHeight = 6f;
+    [Space]
+    [SerializeField] Transform[] _crystalMediumAlter;
+    [Tooltip("This is use for controlling the height of crystals can flow based on LevelHandler.")]
+    [SerializeField] float _crystalMediumAlterHeight = 6f;
+    [Space]
+    [SerializeField] Transform[] _crystalMedium;
+    [Tooltip("This is use for controlling the height of the outer crystals can flow based on LevelHandler.")]
+    [SerializeField] float _crystalMediumHeight = 15f;
+    [Space]
+    [SerializeField] Transform[] _crystalLarge;
+    [Tooltip("This is use for controlling the height of the outer crystals can flow based on LevelHandler.")]
+    [SerializeField] float _crystalLargeHeight = 20f;
 
 
     private void Awake()
@@ -30,6 +46,56 @@ public class Level1Scripts : MonoBehaviour
             crystal.DORotate(new Vector3(360, 360, 360), 3, RotateMode.FastBeyond360)
                    .SetLoops(-1, LoopType.Incremental)
                    .SetEase(Ease.Linear)
+                   .SetUpdate(true);
+        }
+
+        foreach (var crystal in _crystalOuterAlter)
+        {
+            crystal.DOMoveY(_crystalOuterHeight, Random.Range(1.8f, 2f))
+                   .SetLoops(-1, LoopType.Yoyo)
+                   .SetEase(Ease.InOutSine)
+                   .SetUpdate(true);
+            crystal.DORotate(new Vector3(360, 360, 360), 3, RotateMode.FastBeyond360)
+                   .SetLoops(-1, LoopType.Incremental)
+                   .SetEase(Ease.Linear)
+                   .SetUpdate(true);
+        }
+
+        //foreach (var crystalMedium in _crystalMediumAlter)
+        //{
+        //    crystalMedium.DOMoveY(_crystalMediumAlterHeight, Random.Range(1.8f, 2f))
+        //           .SetLoops(-1, LoopType.Yoyo)
+        //           .SetEase(Ease.InOutSine)
+        //           .SetUpdate(true)
+        //           .SetDelay(2f);
+        //}
+
+        for (int i = 0; i < _crystalMediumAlter.Length; i++)
+        {
+            _crystalMediumAlter[i].DOMoveY(_crystalMediumAlterHeight, Random.Range(1.8f, 2f))
+                                  .SetLoops(-1, LoopType.Yoyo)
+                                  .SetEase(Ease.InOutSine)
+                                  .SetUpdate(true)
+                                  .SetDelay(2f);
+        }
+
+        foreach (var crystalMedium in _crystalMedium)
+        {
+            crystalMedium.DOMoveY(_crystalMediumHeight, Random.Range(3f, 4f))
+                   .SetLoops(-1, LoopType.Yoyo)
+                   .SetEase(Ease.OutCirc)
+                   .SetUpdate(true);
+            crystalMedium.DORotate(new Vector3(360, 360, 360), 3, RotateMode.FastBeyond360)
+                   .SetLoops(-1, LoopType.Incremental)
+                   .SetEase(Ease.Linear)
+                   .SetUpdate(true);
+        }
+
+        foreach (var crystalLarge in _crystalLarge)
+        {
+            crystalLarge.DOMoveY(_crystalLargeHeight, Random.Range(3f, 4f))
+                   .SetLoops(-1, LoopType.Yoyo)
+                   .SetEase(Ease.OutCirc)
                    .SetUpdate(true);
         }
     }
