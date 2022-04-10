021301edd3bc05cbd93703d85eb6bb9fb183a879
Kleon <kleon2070310@gmail.com>
2022-04-10 14:21:05 +0800

    Update Skill_WindBall.cs

--- S4Unit3/Assets/_System/Boss/No1/Skill/Skill_WindBall.cs
+++ S4Unit3/Assets/_System/Boss/No1/Skill/Skill_WindBall.cs
@@ -7,6 +7,8 @@ public class Skill_WindBall : MonoBehaviour
     Rigidbody rb;
     [SerializeField] float _fallingSpeed = 3f;
 
+    PlayerState playerState;
+
     void Start()
     {
         //rb = GetComponent<Rigidbody>();
@@ -26,6 +28,17 @@ public class Skill_WindBall : MonoBehaviour
         if (col.transform.tag == "Wall")
         {
             Destroy(gameObject, .1f);
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            if (col.gameObject.GetComponent<CapsuleCollider>().enabled)
+            {
+                playerState = col.gameObject.GetComponent<PlayerState>();
+                playerState.hp_decrease();
+
+                Destroy(gameObject);
+            }
+
         }
     }
 
