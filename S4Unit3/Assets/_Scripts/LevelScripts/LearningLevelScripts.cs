e734898b5f29c5ad56d6cd896a561b89bfbd01bb
pinpinwood <4a7k0084@stust.edu.tw>
2022-05-04 19:18:26 +0800

    emmmm

--- S4Unit3/Assets/_Scripts/LevelScripts/LearningLevelScripts.cs
+++ S4Unit3/Assets/_Scripts/LevelScripts/LearningLevelScripts.cs
@@ -9,6 +9,11 @@ public class LearningLevelScripts : MonoBehaviour
     [SerializeField] GameObject _Portal;
     Animation _LarningTextAnim, _LarningTextRotateAnim;
 
+    [Header("Learning Scence State")]
+    public bool isMove, isDash, isAttack, isCD, isRespawn;
+    bool isP1Move, isP1Dash, isP1Attack, isP1CD;
+    bool isP2Move, isP2Dash, isP2Attack, isP2CD;
+
     // Start is called before the first frame update
     void Start()
     {
