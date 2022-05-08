988344fa9df6bfda01ac9df57e9f9a5183a4b04a
kaiser713 <kleon2070310@gmail.com>
2022-05-08 17:08:41 +0800

    Time line update WIP

---
+++ S4Unit3/Assets/_Scripts/LevelScripts/Level1TimeLine.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class Level1TimeLine : MonoBehaviour
+{
+    public GameObject DeathScene;
+    bool isActive = false;
+
+    public GameObject Boss;
+    Vector3 BossOrgPoint;
+
+    void Awake()
+    {
+        DeathScene.SetActive(false);
+    }
+
+    void Start()
+    {
+        BossOrgPoint = Boss.transform.position;
+    }
+
+    void Update()
+    {
+        if (Level1GameData.b_isBossDeathCutScene && !isActive)
+        {
+            isActive = true;
+
+            Boss.transform.position = BossOrgPoint;
+            DeathScene.SetActive(true);
+        }
+    }
+}
